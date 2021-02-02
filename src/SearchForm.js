c9d960cf9bb341827213120116140798c1e100b6
dm1tro69 <dimolg22@gmail.com>
2021-02-02 22:46:20 +0200

    finished

--- src/SearchForm.js
+++ src/SearchForm.js
@@ -2,7 +2,15 @@ import React from 'react'
 import { useGlobalContext } from './context'
 
 const SearchForm = () => {
-  return <h2>search form</h2>
+  const {handleSearch, query} = useGlobalContext()
+  return <form className={'search-form'} onSubmit={(e)=> e.preventDefault()}>
+    <h2>search hacker news</h2>
+    <input
+        className={'form-input'}
+        type="text"
+        value={query}
+        onChange={(e)=> handleSearch(e.target.value)}/>
+  </form>
 }
 
 export default SearchForm
